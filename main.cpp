fbd2b1072e4064bb271e4c3ca8a4705c9bb16751
Sepehr <sepehr.shamloo3443@gmail.com>
2023-12-13 17:25:01 +0000

    Init Commit Goals Achieved: Made a window using OpenGL Libraries.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // GLFW Library Init
+    if (!glfwInit()) {
+        // GLFW: Init failed
+        cout << "GLFW: Initialization failed.";
+        return -1;
+    }
+
+    // Declaring Window Specifications
+    GLFWwindow* window = glfwCreateWindow(800, 600, "MMO-Engine-TestBuild v.Init",
+                                          nullptr, nullptr);
+
+    if (!window) {
+        // Problem with creating the window.
+        cout << "GLFW: Window creation failed.";
+        glfwTerminate();
+        return -1;
+    }
+
+    // Makes the window the relevant one?
+    glfwMakeContextCurrent(window);
+
+    // While window is not supposed to close.
+    while (!glfwWindowShouldClose(window)){
+        // Render here?
+
+        // Not sure what this does have to look into it and read a bit.
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
