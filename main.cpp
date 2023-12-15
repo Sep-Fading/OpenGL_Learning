aa4652620fe7f863abd36b44defbb87de42bc12d
Sepehr <personal.sepehr@gmail.com>
2023-12-15 14:31:32 +0000

    - Changed the window size to 1920x1080 - Added correct viewport for the renderer - Registered the framebuffer_size_callback to resize the viewport correctly when the window size is  adjusted. - Removed redundant glfwDestroyWindow(window); - Rendered some color on screen - Added function that takes in RGBA floats and clears the screen with that color.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void ClearWithColor(float red, float green, float blue, float alpha);
+
 using namespace std;
 
 // Error call backs for GLFW
@@ -17,6 +19,11 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
+// Viewport resizer based on window adjustments.
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0,0, width, height);
+}
+
 int main() {
     // GLFW Library Init
     if (!glfwInit()) {
@@ -28,7 +35,7 @@ int main() {
     glfwSetErrorCallback(error_callback);
 
     // Declaring Window Specifications
-    GLFWwindow* window = glfwCreateWindow(800, 600, "MMO-Engine-TestBuild v.Init",
+    GLFWwindow* window = glfwCreateWindow(1920, 1080, "MMO-Engine-TestBuild v.Init",
                                           nullptr, nullptr);
 
     if (!window) {
@@ -47,20 +54,34 @@ int main() {
         return -1;
     }
 
+    // Setting the viewport for the renderer.
+    glViewport(0,0,1920,1080);
+    // Letting GLFW know we want to call this function on every window resize by registering it.
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
     // Sets the key_callback function to apply to this window.
     glfwSetKeyCallback(window, key_callback);
 
     // While window is not supposed to close.
     while (!glfwWindowShouldClose(window)){
-        // Render here?
+        // Render here
+        ClearWithColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+
+        // --------------------------------------------------------------------
 
         glfwSwapBuffers(window); // This one swaps back and front buffer, back one is the one you
                                  // render to and front is what's being displayed.
 
-        glfwPollEvents(); // This one still unsure
+        glfwPollEvents(); // Checks if events are triggered, updates window state and calls the corresponding functions.
     }
 
-    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
+}
+
+// Clears the screen with specified color.
+void ClearWithColor(float red, float green, float blue, float alpha) {
+    glClearColor(red, green, blue, alpha);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
